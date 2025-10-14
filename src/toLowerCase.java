65bea822411eb251d4403bec08caef1f4a1f0446
Vysotskyi Mykyta <vysotskiy.n.1995@gmail.com>
2025-10-14 23:59:06 +0300

    add code of realization

--- src/toLowerCase.java
+++ src/toLowerCase.java
@@ -1,3 +1,18 @@
 public class toLowerCase {
+        public static String toLowerCase(String s) {
+            StringBuilder result = new StringBuilder();
 
+            for (char ch : s.toCharArray()) {
+                if (ch >= 'A' && ch <= 'Z') {
+                    result.append((char)(ch + 32));
+                } else {
+                    result.append(ch);
+                }
+            }
+            return result.toString();
+        }
+
+    public static void main(String[] args) {
+        System.out.println(toLowerCase("HELLO"));
+    }
 }
